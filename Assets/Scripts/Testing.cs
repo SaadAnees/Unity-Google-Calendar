0eee9b3bf43eafa5f0a266c33646f9df49c600ec
Saad Anees <saad@pixelplusmedia.com>
2020-10-28 13:31:04 +0400

    Initial implementation of google calender in unity

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleCalendarUnity;
+
+public class Testing : MonoBehaviour
+{
+    public Calendar calendar = new Calendar();
+    
+    private void Update()
+    {
+        calendar.Main();
+    }
+
+}
